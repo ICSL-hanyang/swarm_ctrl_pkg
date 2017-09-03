28f39be26fc4d238b81cdeb0b441e8fa0962273a
Sewoong Min <gosupia@naver.com>
2017-09-04 07:07:27 +0900

    set_home node && set_vel added

--- src/set_pos_local.cpp
+++ src/set_pos_local.cpp
@@ -1,17 +1,19 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h> //set position 용
+#include <geometry_msgs/TwistStamped.h> //set position 용
 #include "swarm_ctrl_pkg/srvMultiSetPosLocal.h"
 #define NUM_DRONE 5
 
-double pre_req[4] = {0.0, 0.0, 0.0, 2.0};
+double pre_req[7] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0};
 bool b_sending = false;
 geometry_msgs::PoseStamped l_pos[NUM_DRONE];
+geometry_msgs::TwistStamped l_vel[NUM_DRONE];
 
 bool multiSetPosLocal(swarm_ctrl_pkg::srvMultiSetPosLocal::Request &req, 
 	swarm_ctrl_pkg::srvMultiSetPosLocal::Response &res){
 	b_sending = req.sending;
 	if(req.sending){
-		if (req.x != pre_req[0] | req.y != pre_req[1] | req.z != pre_req[2] | req.offset != pre_req[3]){
+		if (req.x != pre_req[0] | req.y != pre_req[1] | req.z != pre_req[2] | req.vel_x != pre_req[3] | req.vel_y != pre_req[4] | req.vel_z != pre_req[5] | req.offset != pre_req[6]){
 			l_pos[0].pose.position.x = req.x;
 			l_pos[0].pose.position.y = req.y;
 			l_pos[0].pose.position.z = req.z;
@@ -22,6 +24,9 @@ bool multiSetPosLocal(swarm_ctrl_pkg::srvMultiSetPosLocal::Request &req,
 
 			for (int i = 1; i < NUM_DRONE; i++){
 				l_pos[i] = l_pos[0];
+				l_vel[i].twist.linear.x = req.vel_x;
+				l_vel[i].twist.linear.y = req.vel_y;
+				l_vel[i].twist.linear.z = req.vel_z;
 				if(i < 3)
 					l_pos[i].pose.position.x += req.offset;
 				else if(i < 5){
@@ -29,7 +34,7 @@ bool multiSetPosLocal(swarm_ctrl_pkg::srvMultiSetPosLocal::Request &req,
 				}
 				req.offset *= -1;
 			}
-			ROS_INFO("(%lf, %lf, %lf), offset : %lf", req.x, req.y, req.z, req.offset);
+			ROS_INFO("move(%lf, %lf, %lf), vel(%lf, %lf, %lf) offset : %lf", req.x, req.y, req.z, req.vel_x, req.vel_y, req.vel_z, req.offset);
 			res.success = true;
 		}
 		else{
@@ -47,15 +52,19 @@ int main(int argc, char** argv){
 
 	ros::NodeHandle nh;
 	ros::Publisher local_pos_pub[NUM_DRONE];
+	ros::Publisher local_vel_pub[NUM_DRONE];
 	ros::ServiceServer multi_set_pos_local_server = nh.advertiseService("multi_set_pos_local", multiSetPosLocal);
 
 	std::stringstream stream;  
 	std::string group_name = "camila";
 	std::string d_mavros_l_pos = "/mavros/setpoint_position/local";
+	std::string d_mavros_l_vel = "/mavros/setpoint_velocity/cmd_vel";
 	for(int i=0 ; i < NUM_DRONE ; i++){
 		stream << i;
 		local_pos_pub[i] = nh.advertise<geometry_msgs::PoseStamped>(
 			group_name + stream.str() + d_mavros_l_pos, 10);
+		local_vel_pub[i] = nh.advertise<geometry_msgs::TwistStamped>(
+			group_name + stream.str() + d_mavros_l_vel, 10);
 		stream.str("");
 	}
 	ros::Rate rate(20.0); // period 0.005 s
@@ -65,6 +74,7 @@ int main(int argc, char** argv){
 		if(b_sending){
 			for (int i = 0; i < NUM_DRONE; i++){
 				local_pos_pub[i].publish(l_pos[i]);
+				local_vel_pub[i].publish(l_vel[i]);
 			}
 		}
 		ros::spinOnce();
--- src/state.cpp
+++ src/state.cpp
@@ -7,23 +7,23 @@ typedef void (*FuncPtr)(const mavros_msgs::State::ConstPtr&);
 mavros_msgs::State current_state[NUM_DRONE];
 std::string mode[NUM_DRONE];
 
-void state_cb0(const mavros_msgs::State::ConstPtr& msg){
+void stateCB0(const mavros_msgs::State::ConstPtr& msg){
 	current_state[0] = *msg;
 	mode[0]=current_state[0].mode;
 }
-void state_cb1(const mavros_msgs::State::ConstPtr& msg){
+void stateCB1(const mavros_msgs::State::ConstPtr& msg){
 	current_state[1] = *msg;
 	mode[1]=current_state[1].mode;
 }
-void state_cb2(const mavros_msgs::State::ConstPtr& msg){
+void stateCB2(const mavros_msgs::State::ConstPtr& msg){
 	current_state[2] = *msg;
 	mode[2]=current_state[2].mode;
 }
-void state_cb3(const mavros_msgs::State::ConstPtr& msg){
+void stateCB3(const mavros_msgs::State::ConstPtr& msg){
 	current_state[3] = *msg;
 	mode[3]=current_state[3].mode;
 }
-void state_cb4(const mavros_msgs::State::ConstPtr& msg){
+void stateCB4(const mavros_msgs::State::ConstPtr& msg){
 	current_state[4] = *msg;
 	mode[4]=current_state[4].mode;
 }
@@ -39,7 +39,7 @@ int main(int argc, char** argv){
 	std::string group_name = "camila";
 	std::string d_mavros_state = "/mavros/state";
 
-	FuncPtr stateFP[NUM_DRONE] = {state_cb0, state_cb1, state_cb2, state_cb3, state_cb4};
+	FuncPtr stateFP[NUM_DRONE] = {stateCB0, stateCB1, stateCB2, stateCB3, stateCB4};
 	mavros_msgs::State pre_state[NUM_DRONE];
 
 	for(int i=0 ; i < NUM_DRONE ; i++){
@@ -50,7 +50,12 @@ int main(int argc, char** argv){
 	}
 
 	ros::Rate rate(20.0); // period 0.005
-	
+	for(int i=0 ; i< NUM_DRONE; i++){
+		pre_state[i].connected = true;
+		pre_state[i].mode = "OFFBOARD";
+		pre_state[i].armed = true;
+	}
+
 	ROS_INFO("State check start");
 	while(ros::ok()){
 		int cnt_connected = 0;
@@ -81,16 +86,16 @@ int main(int argc, char** argv){
 				}
 				
 			}
+			pre_state[i] = current_state[i];
 		}
 		(cnt_connected == NUM_DRONE) ? msg.connected = true : msg.connected = false;
 		(cnt_mode == NUM_DRONE) ? msg.mode = true : msg.mode = false;
 		(cnt_armed == NUM_DRONE) ? msg.armed = true : msg.armed = false;
-		*pre_state = *current_state;
+		
 		multi_state_pub.publish(msg);
 
 		ros::spinOnce();
 		rate.sleep();
 	}
-
 	return 0;
 }
