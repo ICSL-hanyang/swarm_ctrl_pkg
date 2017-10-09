6ab4fe9260125a8f5402c2b9663bd20bf9502804
Sewoong Min <gosupia@naver.com>
2017-10-09 17:20:46 +0900

    cmd

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -30,157 +30,113 @@ double takeoff_alt = 2.5;
 
 
 bool multiArming(swarm_ctrl_pkg::srvMultiArming::Request& req,
-                 swarm_ctrl_pkg::srvMultiArming::Response& res)
+	swarm_ctrl_pkg::srvMultiArming::Response& res)
 {
-  int arming_cnt = 0;
-  mavros_msgs::CommandBool arm_cmd;
-  arm_cmd.request.value = req.arming;
+	int arming_cnt = 0;
+	mavros_msgs::CommandBool arm_cmd;
+	arm_cmd.request.value = req.arming;
 
-  for (int i = 0; i < NUM_DRONE; i++)
-  {
-    if (arming_client[i].call(arm_cmd) && arm_cmd.response.success)
-    {
-      arming_cnt++;
-    }
-    else
-    {
-      arming_cnt = 0;
-    }
-  }
-  if (arming_cnt == NUM_DRONE)
-  {
-    res.success = true;
-  }
-  else
-  {
-    for (int i = 0; i < NUM_DRONE; i++)
-    {
-      arm_cmd.request.value = false;
-      if (arming_client[i].call(arm_cmd) && arm_cmd.response.success)
-      {
-      }
-    }
-    ROS_WARN("Arming fail. All drones are disarmed");
-    res.success = false;
-  }
-  return true;
+	if (arming_client[NUM_DRONE].call(arm_cmd) && arm_cmd.response.success){
+		res.success = true;
+	}
+	else
+	{
+		ROS_WARN("Arming fail. Camila%d are disarmed", NUM_DRONE);
+		res.success = false;
+	}
+	return true;
 }
 
 bool multiMode(swarm_ctrl_pkg::srvMultiMode::Request& req,
-               swarm_ctrl_pkg::srvMultiMode::Response& res)
+	swarm_ctrl_pkg::srvMultiMode::Response& res)
 {
-  int mode_cnt = 0;
-  swarm_ctrl_pkg::srvMultiSetPosLocal p_msg;
-  mavros_msgs::SetMode set_mode;
-  p_msg.request.pos_flag = true;
-  p_msg.request.x = 0;
-  p_msg.request.y = 0;
-  p_msg.request.z = takeoff_alt;
+	int mode_cnt = 0;
+	swarm_ctrl_pkg::srvMultiSetPosLocal p_msg;
+	mavros_msgs::SetMode set_mode;
+	p_msg.request.pos_flag = true;
+	p_msg.request.x = 0;
+	p_msg.request.y = 0;
+	p_msg.request.z = takeoff_alt;
 
-  set_mode.request.custom_mode = req.mode;
-  for (int i = 0; i < NUM_DRONE; i++)
-  {
-    if (set_mode_client[i].call(set_mode) && set_mode.response.success)
-    {
-      mode_cnt++;
-    }
-    else
-    {
-      mode_cnt = 0;
-    }
-  }
-  multi_set_pos_local_client.call(p_msg);
-  if (mode_cnt == NUM_DRONE)
-  {
-    res.success = true;
-  }
-  else
-  {
-    res.success = false;
-  }
-  return true;
+	set_mode.request.custom_mode = req.mode;
+	if (set_mode_client[NUM_DRONE].call(set_mode) && set_mode.response.success)
+	{
+		res.success = true;
+	}
+	else
+	{
+		res.success = false;
+	}
+	multi_set_pos_local_client.call(p_msg);
+	return true;
 }
 
 
 void LocalPosCB(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
-  std::stringstream stream;
-  for (int i = 0; i < NUM_DRONE; i++)
-  {
-    stream << i;
-    if (msg->header.frame_id == group_name + stream.str())
-      l_pos[i] = *msg;
-    stream.str("");
-  }
+	l_pos[NUM_DRONE] = *msg;
 }
 void globalPosCB(const sensor_msgs::NavSatFix::ConstPtr& msg)
 {
-  std::stringstream stream;
-  for (int i = 0; i < NUM_DRONE; i++)
-  {
-    stream << i;
-    if (msg->header.frame_id == group_name + stream.str())
-      g_pos[i] = *msg;
-    stream.str("");
-  }
+	g_pos[NUM_DRONE] = *msg;
 }
 
 void multiStateCB(const swarm_ctrl_pkg::msgState::ConstPtr& msg)
 {
-  multi_state = *msg;
+	multi_state = *msg;
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "cmd_node");
-  ros::NodeHandle nh;
+	ros::init(argc, argv, "cmd_node");
+	ros::NodeHandle nh;
 
-  ros::Subscriber local_pos_sub[NUM_DRONE];
-  ros::Subscriber global_pos_sub[NUM_DRONE];
-  ros::ServiceServer multi_arming_server =
-      nh.advertiseService("multi_arming", multiArming);
-  ros::ServiceServer multi_mode_server =
-      nh.advertiseService("multi_mode", multiMode);
-  ros::Subscriber multi_state_sub =
-      nh.subscribe("multi_state", 50, multiStateCB);
+	ros::Subscriber local_pos_sub[NUM_DRONE];
+	ros::Subscriber global_pos_sub[NUM_DRONE];
+	ros::ServiceServer multi_arming_server =
+	nh.advertiseService("multi_arming", multiArming);
+	ros::ServiceServer multi_mode_server =
+	nh.advertiseService("multi_mode", multiMode);
+	ros::Subscriber multi_state_sub =
+	nh.subscribe("multi_state", 50, multiStateCB);
 
-  multi_set_pos_local_client =
-      nh.serviceClient<swarm_ctrl_pkg::srvMultiSetPosLocal>(
-          "multi_set_pos_local");
-  multi_set_vel_local_client =
-      nh.serviceClient<swarm_ctrl_pkg::srvMultiSetVelLocal>(
-          "multi_set_vel_local");
+	multi_set_pos_local_client =
+	nh.serviceClient<swarm_ctrl_pkg::srvMultiSetPosLocal>(
+		"multi_set_pos_local");
+	multi_set_vel_local_client =
+	nh.serviceClient<swarm_ctrl_pkg::srvMultiSetVelLocal>(
+		"multi_set_vel_local");
 
-  std::stringstream stream;
-  std::string d_mavros_arm = "/mavros/cmd/arming";
-  std::string d_mavros_mode = "/mavros/set_mode";
+	std::stringstream stream;
+	std::string d_mavros_arm = "/mavros/cmd/arming";
+	std::string d_mavros_mode = "/mavros/set_mode";
 
-  std::string d_mavros_l_pos = "/mavros/local_position/pose";
-  std::string d_mavros_g_pos = "/mavros/global_position/global";
+	std::string d_mavros_l_pos = "/mavros/local_position/pose";
+	std::string d_mavros_g_pos = "/mavros/global_position/global";
 
-  for (int i = 0; i < NUM_DRONE; i++)
-  {
-    stream << i;
-    arming_client[i] = nh.serviceClient<mavros_msgs::CommandBool>(
-        group_name + stream.str() + d_mavros_arm);
-    set_mode_client[i] = nh.serviceClient<mavros_msgs::SetMode>(
-        group_name + stream.str() + d_mavros_mode);
-    local_pos_sub[i] = nh.subscribe(group_name + stream.str() + d_mavros_l_pos,
-                                    10, LocalPosCB);
-    global_pos_sub[i] = nh.subscribe(group_name + stream.str() + d_mavros_g_pos,
-                                     10, globalPosCB);
-    stream.str("");
-  }
+	for (int i = 0; i < NUM_DRONE; i++)
+	{
+		stream << i;
+		arming_client[i] = nh.serviceClient<mavros_msgs::CommandBool>(
+			group_name + stream.str() + d_mavros_arm);
+		set_mode_client[i] = nh.serviceClient<mavros_msgs::SetMode>(
+			group_name + stream.str() + d_mavros_mode);
+		local_pos_sub[i] = nh.subscribe(group_name + stream.str() + d_mavros_l_pos,
+			10, LocalPosCB);
+		global_pos_sub[i] = nh.subscribe(group_name + stream.str() + d_mavros_g_pos,
+			10, globalPosCB);
+		stream.str("");
+	}
 
   ros::Rate rate(10.0); // period 0.01 s
   ROS_INFO("Command node started");
 
   while (ros::ok()){
 
-    nh.getParam("cmd_node/takeoff_alt", takeoff_alt);
+  	nh.getParam("cmd_node/takeoff_alt", takeoff_alt);
 
-    ros::spinOnce();
-    rate.sleep();
+  	ros::spinOnce();
+  	rate.sleep();
   }
 
 
