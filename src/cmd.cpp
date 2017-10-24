551757f92f49503b3e37523c5ba99e5de5cf8f9c
Sewoong Min <gosupia@naver.com>
2017-10-24 14:48:39 +0900

    oo

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -12,7 +12,7 @@
 #include "swarm_ctrl_pkg/srvMultiSetHome.h"
 #include "swarm_ctrl_pkg/srvMultiLanding.h"
 
-#define NUM_DRONE 1
+#define NUM_DRONE 2
 
 ros::ServiceClient arming_client[5];
 ros::ServiceClient set_mode_client[5];
