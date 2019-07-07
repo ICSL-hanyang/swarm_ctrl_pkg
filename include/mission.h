e4070c77a586f7f8537e53bfccda564fdacbcc0f
sewoongmin <gosupia@naver.com>
2019-07-07 19:10:18 +0900

    misson structure added

---
+++ include/mission.h
@@ -0,0 +1,28 @@
+#ifndef MISSION_H
+#define MISSION_H
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <ros/ros.h>
+#include <tf/tf.h>
+#include <tf2/LinearMath/Quaternion.h>
+
+class Mission
+{
+private:
+	ros::Subscriber local_pos_sub_;
+    std::vector<tf2::Vector3> waypoints_;
+    tf2::Vector3 cur_waypoint_;
+	geometry_msgs::PoseStamped cur_local_;
+	void localPositionCB(const geometry_msgs::PoseStamped::ConstPtr &);
+
+public:
+    Mission();
+    ~Mission();
+    void clear();
+    void pushWaypoint(tf2::Vector3 &);
+    bool checkReached() const;
+};
+
+#endif
