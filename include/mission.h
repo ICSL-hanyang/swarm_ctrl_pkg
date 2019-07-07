787f933d2e4d6af5ef2fba6c829cfcb94a4cbce7
sewoongmin <gosupia@naver.com>
2019-07-08 03:24:40 +0900

    mission node complete

--- include/mission.h
+++ include/mission.h
@@ -7,12 +7,17 @@
 #include <ros/ros.h>
 #include <tf/tf.h>
 #include <tf2/LinearMath/Quaternion.h>
+#include <swarm_ctrl_pkg/srvMultiSetpointLocal.h>
 
 class Mission
 {
 private:
+    ros::NodeHandle nh_;
 	ros::Subscriber local_pos_sub_;
+    ros::ServiceClient setpoint_client_;
     std::vector<tf2::Vector3> waypoints_;
+    int wp_index_;
+    double initial_yaw_;
     tf2::Vector3 cur_waypoint_;
 	geometry_msgs::PoseStamped cur_local_;
 	void localPositionCB(const geometry_msgs::PoseStamped::ConstPtr &);
@@ -21,8 +26,10 @@ public:
     Mission();
     ~Mission();
     void clear();
-    void pushWaypoint(tf2::Vector3 &);
-    bool checkReached() const;
+    void pushWaypoint(const tf2::Vector3 &);
+    bool checkReached();
+    void findYaw();
+    void run();
 };
 
 #endif
--- src/mission.cpp
+++ src/mission.cpp
@@ -1,27 +1,82 @@
 #include <mission.h>
 
-Mission::Mission()
+Mission::Mission() : 
+    nh_(ros::NodeHandle("")),
+    wp_index_(-1),
+    initial_yaw_(0),
+    cur_waypoint_(tf2::Vector3(0,0,0))
 {
+	local_pos_sub_ = nh_.subscribe("/camila1/mavros/local_position/pose", 5, &Mission::localPositionCB, this);
+	setpoint_client_ = nh_.serviceClient<swarm_ctrl_pkg::srvMultiSetpointLocal>("/multi_setpoint_local");
 }
 
 Mission::~Mission()
 {
+    local_pos_sub_.shutdown();
+    setpoint_client_.shutdown();
 }
+
+void Mission::localPositionCB(const geometry_msgs::PoseStamped::ConstPtr &msg){
+    cur_local_ = *msg;
+}
+
 
 void Mission::clear(){
     waypoints_.clear();
 }
 
-void Mission::pushWaypoint(tf2::Vector3 &waypoint){
+void Mission::pushWaypoint(const tf2::Vector3 &waypoint){
     waypoints_.push_back(waypoint);
 }
 
-bool Mission::checkReached() const
+bool Mission::checkReached()
 {
-    if( (abs(cur_waypoint_.getX() - cur_local_.pose.position.x) < 0.5) && 
-        (abs(cur_waypoint_.getY() - cur_local_.pose.position.y) < 0.5) &&
-        (abs(cur_waypoint_.getZ() - cur_local_.pose.position.z) < 0.5) )
+    findYaw();
+    double c_x, c_y, c_z, rotated_x, rotated_y;
+    c_x = cur_waypoint_.getX();
+    c_y = cur_waypoint_.getY();
+    c_z = cur_waypoint_.getZ();
+    rotated_x = c_x * cos(initial_yaw_) - c_y*sin(initial_yaw_);
+    rotated_y = c_x * sin(initial_yaw_) + c_y*cos(initial_yaw_);
+    if( (abs(rotated_x - cur_local_.pose.position.x) < 0.7) && 
+        (abs(rotated_y - cur_local_.pose.position.y) < 0.7) &&
+        (abs(c_z - cur_local_.pose.position.z) < 0.7) )
         return true;
     else
         return false;
+}
+ 
+void Mission::findYaw(){
+    if(initial_yaw_ == 0)
+    {
+        tf::Quaternion q(
+            cur_local_.pose.orientation.x,
+            cur_local_.pose.orientation.y,
+            cur_local_.pose.orientation.z,
+            cur_local_.pose.orientation.w);
+        tf::Matrix3x3 m(q);
+        double r, p, y;
+        m.getRPY(r, p, y);
+        initial_yaw_ = y;
+        ROS_INFO_STREAM(initial_yaw_);
+    }
+}
+
+void Mission::run()
+{
+    if(checkReached())
+    {
+        wp_index_++;
+        if(waypoints_.size() > wp_index_)
+            cur_waypoint_ = waypoints_.at(wp_index_);
+        swarm_ctrl_pkg::srvMultiSetpointLocal msg;
+        msg.request.formation = "POINT";
+        msg.request.x = cur_waypoint_.getX();
+        msg.request.y = cur_waypoint_.getY();
+        msg.request.z = cur_waypoint_.getZ();
+
+        if(setpoint_client_.call(msg) && msg.response.success);
+        else
+            ROS_INFO("Can not call next setpoint!");
+    }
 }
